41b7a62888bd21acda4e8d65284491b9a9da7d8a
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2026-02-15 14:58:31 +0900

    feat: Userエンティティを追加

---
+++ src/main/java/com/app/chronolog/entity/User.java
@@ -0,0 +1,37 @@
+package com.app.chronolog.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Setter
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String employeeId;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private boolean enabled;
+}
