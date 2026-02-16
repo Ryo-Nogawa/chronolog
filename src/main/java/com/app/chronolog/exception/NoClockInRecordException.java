1aad8e6ecfaaa6255adbb1384ca3eb72195b86e8
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2026-02-16 13:06:15 +0900

    feat: NoClockInRecordExceptionを追加

---
+++ src/main/java/com/app/chronolog/exception/NoClockInRecordException.java
@@ -0,0 +1,8 @@
+package com.app.chronolog.exception;
+
+public class NoClockInRecordException extends RuntimeException {
+
+    public NoClockInRecordException(String message) {
+        super(message);
+    }
+}
