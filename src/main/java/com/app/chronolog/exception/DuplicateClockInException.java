762cbac767ac457604c93611fe29c92536a54a42
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2026-02-16 13:04:28 +0900

    feat: DuplicateClockInExceptionを追加

---
+++ src/main/java/com/app/chronolog/exception/DuplicateClockInException.java
@@ -0,0 +1,8 @@
+package com.app.chronolog.exception;
+
+public class DuplicateClockInException extends RuntimeException {
+
+    public DuplicateClockInException(String message) {
+        super(message);
+    }
+}
