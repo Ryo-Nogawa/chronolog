56fef46ba4d1264b985490b8b62afc684443ec06
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2026-02-16 13:58:41 +0900

    feat: 退勤打刻機能（clockOut）を実装

--- src/main/java/com/app/chronolog/service/AttendanceServiceImpl.java
+++ src/main/java/com/app/chronolog/service/AttendanceServiceImpl.java
@@ -2,11 +2,13 @@ package com.app.chronolog.service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.app.chronolog.entity.AttendanceRecord;
 import com.app.chronolog.exception.DuplicateClockInException;
+import com.app.chronolog.exception.NoClockInRecordException;
 import com.app.chronolog.repository.AttendanceRepository;
 
 @Service
@@ -33,5 +35,21 @@ public class AttendanceServiceImpl implements AttendanceService {
         input.setClockInTime(LocalDateTime.now());
 
         return attendanceRepository.save(input);
+    }
+
+    @Override
+    public AttendanceRecord clockOut(String employeeId) {
+        // 出勤記録チェック
+        LocalDate today = LocalDate.now();
+        Optional<AttendanceRecord> clockInInfo = attendanceRepository.findByEmployeeIdAndWorkDate(employeeId, today);
+        if (clockInInfo.isEmpty()) {
+            throw new NoClockInRecordException("従業員ID: " + employeeId + "は本日の出勤記録が作成されていません");
+        }
+
+        // 退勤記録を作成
+        AttendanceRecord createdClockInfo = clockInInfo.get();
+        createdClockInfo.setClockOutTime(LocalDateTime.now());
+
+        return attendanceRepository.save(createdClockInfo);
     }
 }
