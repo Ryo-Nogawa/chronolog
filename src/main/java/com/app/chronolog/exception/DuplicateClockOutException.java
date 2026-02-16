d0614df17931d191ba0f3850606d27017388dd27
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2026-02-16 13:08:52 +0900

    feat: DuplicateClockOutExceptionを追加

---
+++ src/main/java/com/app/chronolog/exception/DuplicateClockOutException.java
@@ -0,0 +1,8 @@
+package com.app.chronolog.exception;
+
+public class DuplicateClockOutException extends RuntimeException {
+
+    public DuplicateClockOutException(String message) {
+        super(message);
+    }
+}
